f6ac760c8887f0cea77ac2eaf502d2de2f7ed34b
vladimirkapereula <vpereula@gmail.com>
2022-04-14 14:26:36 -0700

    update for aos_locators.py

--- aos_locators.py
+++ aos_locators.py
@@ -1,14 +1,12 @@
-
 from faker import Faker
 fake = Faker(locale='en_CA')
-
 aos_url = 'https://www.advantageonlineshopping.com/#/'  #/html/body/login-modal/div/div/div[3]/sec-form
 aos_title = ' Advantage Shopping'
 aos_login_url = 'https://advantageonlineshopping.com/#/loginMiniTitle/'
 aos_register_url = 'https://advantageonlineshopping.com/#/register'
-aos_username = "simon"
+aos_username = "simon"[:15]
 aos_password = "Dale12345"[:12]
-new_username = fake.user_name()
+new_username = f'{fake.user_name()}{fake.pyint(111,999)}'[:15]
 email = fake.email()
 new_password = fake.password()
 first_name = fake.first_name()
@@ -22,3 +20,5 @@ state_province_region = fake.province_abbr()
 address = fake.address().replace('\n','')[:49]
 address1 = f'{fake.street_address()}'[:49]
 address2 = f'{fake.street_address()},{city},{fake.province_abbr},{fake.postalcode_in_province},{country}'[:49]
+#order_number = fake.order_number()
+#tracking_number = fake.order_number()
